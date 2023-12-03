185340c3a300b3788bf414268064f26b907867dd
Mmed <ed_alh@icloud.com>
2023-12-03 10:09:00 -0800

    7-add_tuple.py added

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+
+    if len(list_a) < 2:
+        for i in range(len(list_a), 2):
+            list_a.append(0)
+
+    if len(list_b) < 2:
+        for i in range(len(list_b), 2):
+            list_b.append(0)
+
+    list_c = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
+    return (tuple(list_c))
